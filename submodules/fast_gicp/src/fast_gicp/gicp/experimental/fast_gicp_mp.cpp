1245bf4b0d1576826c1401471392b1231583a25e
Riboha <sobo3607@naver.com>
2024-03-22 01:22:17 +0900

    submodules

--- submodules/fast_gicp/src/fast_gicp/gicp/experimental/fast_gicp_mp.cpp
+++
@@ -1,5 +0,0 @@
-#include <fast_gicp/gicp/experimental/fast_gicp_mp.hpp>
-#include <fast_gicp/gicp/experimental/fast_gicp_mp_impl.hpp>
-
-// template class fast_gicp::FastGICPMultiPoints<pcl::PointXYZ, pcl::PointXYZ>;
-template class fast_gicp::FastGICPMultiPoints<pcl::PointXYZI, pcl::PointXYZI>;
